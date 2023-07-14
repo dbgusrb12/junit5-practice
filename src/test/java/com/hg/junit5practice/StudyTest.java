187d40dc209a948d993ad7e72c7d5724b72a5a4a
hyungyu <yhg8484@naver.com>
2023-07-14 17:43:36 +0900

    feat: JUnit5 test 어노테이션 추가

--- src/test/java/com/hg/junit5practice/StudyTest.java
+++ src/test/java/com/hg/junit5practice/StudyTest.java
@@ -2,6 +2,11 @@ package com.hg.junit5practice;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class StudyTest {
@@ -10,5 +15,32 @@ class StudyTest {
     void create() {
         Study study = new Study();
         assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test // 테스트용 로직
+    @Disabled // 테스트 제외
+    void create1() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll // 모든 테스트가 실행되기 전 실행되는 로직 (static void 로 작성, 메서드 이름은 상관 없음)
+    static void beforeAll() {
+        System.out.println("Before all");
+    }
+
+    @AfterAll // 모든 테스트가 실행 된 후 실행되는 로직 (static void 로 작성, 메서드 이름은 상관 없음)
+    static void afterAll() {
+        System.out.println("After all");
+    }
+
+    @BeforeEach // 각각의 테스트가 실행되기 이전에 실행되는 로직
+    void beforeEach() {
+        System.out.println("Before each");
+    }
+
+    @AfterEach // 각각의 테스트가 실행 된 후에 실행되는 로직
+    void afterEach() {
+        System.out.println("After each");
     }
 }
