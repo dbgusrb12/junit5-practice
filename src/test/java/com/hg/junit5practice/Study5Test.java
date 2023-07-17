d9d4f048528ab9ec90c72aed8666102a7e146e8b
hyungyu <yhg8484@naver.com>
2023-07-17 09:54:57 +0900

    feat: 반복 테스트

---
+++ src/test/java/com/hg/junit5practice/Study5Test.java
@@ -0,0 +1,46 @@
+package com.hg.junit5practice;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class Study5Test {
+
+    @RepeatedTest(10)
+    @DisplayName("반복 테스트")
+    void repeat_test() {
+        System.out.println("test");
+    }
+
+    @RepeatedTest(10)
+    @DisplayName("반복 정보 포함 테스트")
+    void repeat_test_with_arguments(RepetitionInfo repetitionInfo) {
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("반복 테스트 이름 변경")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition} / {totalRepetitions}")
+    void repeat_test_with_name(RepetitionInfo repetitionInfo) {
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("파라미터 포함 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"날씨가", "많이", "더워지고", "있네요."})
+    void parameterized_test(String message) {
+        System.out.println(message);
+    }
+
+    @DisplayName("파라미터 포함 테스트 이름 변경")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "더워지고", "있네요."})
+    void parameterized_test_name(String message) {
+        System.out.println(message);
+    }
+}
